58f697e5c16ecc209cc04384b73348e848a92f04
Aldo Velasquez <AF15189@LC02WL10WHTDD>
2019-07-21 23:01:33 -0400

    unit tested tribute

--- projects/velasqal/dominion/unittest4.c
+++ projects/velasqal/dominion/unittest4.c
@@ -5,8 +5,18 @@
 #include <assert.h>
 #include "rngs.h"
 
+#define NOISY_TEST 1
 
 int main() {
+  int randomSeed = 1000;
+  struct gameState gamestate;
+  int kingdomCards[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
+            sea_hag, tribute, smithy};
+  int player1 = 0;
 
+  #if (NOISY_TEST == 1)
+    printf("Testing tribute function with choice 1.\n");
+  #endif
+  initializeGame(2, kingdomCards, randomSeed, &gamestate);
   return 0;
 }
