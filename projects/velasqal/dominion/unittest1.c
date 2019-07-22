5c75c5a489dea62f5e5c2d7f055da5902a53b564
Aldo Velasquez <AF15189@LC02WL10WHTDD>
2019-07-21 21:42:43 -0400

    unit tested baron

--- projects/velasqal/dominion/unittest1.c
+++ projects/velasqal/dominion/unittest1.c
@@ -5,8 +5,40 @@
 #include <assert.h>
 #include "rngs.h"
 
+#define NOISY_TEST 1
 
 int main() {
+  int randomSeed = 1000;
+  struct gameState gamestate;
+  int kingdomCards[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
+            sea_hag, tribute, smithy};
+  int player1 = 0;
+
+  #if (NOISY_TEST == 1)
+    printf("Testing baron function with 2 players.\n");
+  #endif
+  initializeGame(2, kingdomCards, randomSeed, &gamestate);
+  gamestate.hand[player1][0] = estate;
+
+  int handCount = gamestate.handCount[player1];
+  executeBaron(1, &gamestate, player1);
+
+  #if (NOISY_TEST == 1)
+    printf("Expect number of buys increased by 1.\n");
+  #endif
+  assert(gamestate.numBuys == 2);
+
+  #if (NOISY_TEST == 1)
+    printf("Expect number of coins increased by 4.\n");
+  #endif
+  assert(gamestate.coins == 6);
+
+ #if (NOISY_TEST == 1)
+    printf("Expect player1 hand count decreased by 1.\n");
+  #endif
+  assert(gamestate.handCount[player1] == (handCount - 1));
+
+  printf("All tests passed!\n");
 
   return 0;
 }
