3ffd855c32eb632192732220f8594e76494b2560
Aldo Velasquez <AF15189@LC02WL10WHTDD>
2019-07-28 23:28:14 -0400

    fixed again

--- projects/velasqal/dominion/randomtestcard1.c
+++ projects/velasqal/dominion/randomtestcard1.c
@@ -23,16 +23,20 @@ int main() {
     printf("Testing Baron Refactored function.\n");
   #endif
 
-  for (i = 0; i < 900; i++){
+  for (i = 0; i < 500; i++){
     // set random inputs.
-    int numPlayers = (rand() % 3) + 2; // sets number of players 2 to 4.
     int choice1 = (rand() % 2);
-
+    int choice2 = (rand() % 2);
+    int numPlayers = (rand() % 3) + 2; // sets number of players 2 to 4.
     initializeGame(numPlayers, kingdomCards, randomSeed, &postGameState);
     
-    for (j = 0; j < numPlayers; j++){
-      currentPlayer = whoseTurn(&postGameState);
+    for (j = 0; j < 200; j++){
+      currentPlayer = (rand() % numPlayers);
       memcpy(&preGameState, &postGameState, sizeof(struct gameState));// copy post game state to pre game state for later test.
+      
+      if(choice2 == 0){
+        postGameState.supplyCount[estate]--;
+      }
       executeBaron(choice1, &postGameState, currentPlayer);
 
       /**************************** TEST CASES *****************************/
@@ -41,6 +45,20 @@ int main() {
         printf("Number of game state buys increased by one: Passed!\n");
       }else{
         printf("Number of game state buys increased by one: Failed!\n");
+      }
+
+      if(choice1 > 0 && preGameState.hand[currentPlayer][j] == estate){
+        if((preGameState.coins + 4) == postGameState.coins){
+          printf("Number of game state coins increased by 4: Passed!\n");
+        }else{
+          printf("Number of game state coins increased by 4: Failed!\n");
+        }
+
+        if((preGameState.handCount[currentPlayer] - 1) == postGameState.handCount[currentPlayer]){
+          printf("player %i's of handCount decreased by 1: Passed!\n", (currentPlayer + 1));
+        }else{
+          printf("player %i's of handCount decreased by 1: Failed!\n", (currentPlayer + 1));
+        }
       }
     }
   }
